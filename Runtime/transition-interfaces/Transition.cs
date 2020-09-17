7ebe147ead95034f36b875ea670d7b8e3428a448
larry kirschner <larrykirschner@gmail.com>
2020-09-16 23:35:34 -0700

    feature: shows define TRANSITIONS_DEBUG_ENABLED in defines window

--- Runtime/transition-interfaces/Transition.cs
+++ Runtime/transition-interfaces/Transition.cs
@@ -1,4 +1,5 @@
 using System;
+using BeatThat.Defines;
 using BeatThat.Requests;
 
 namespace BeatThat.Transitions
@@ -12,72 +13,91 @@ namespace BeatThat.Transitions
     /// Common examples are transitions to bring in or send out a UI panel.
     /// </summary>
     public interface Transition : Request
-	{
-		event Action<Transition> Completed; // TODO: replace with UnityEvent, possibly have a no-arg event option (for auto unbind)
-		
-		/// <summary>
-		/// Name/id for this transition
-		/// </summary>
-		string transitionName { get; set; }
-		
-		/// <summary>
-		/// Starts and runs the transition
-		/// </summary>
-		void StartTransition();
-		
-		/// <summary>
-		/// Starts the transition using the GlobalTransitionRunner
-		/// </summary>
-		/// <param name='time'>
-		/// Generally use Time.time, but having time as a param allow alternative time models
-		/// </param>
-		void StartTransition(float time);
-		
-		/// <summary>
-		/// Starts the transition leaving the option to *NOT*
-		/// add the transition to the GlobalTransitionRunner.
-		/// This allows a transitions to start and update/control child transitions.
-		/// </summary>
-		/// <param name='time'>
-		/// Time.
-		/// </param>
-		/// <param name='andRun'>
-		/// If TRUE, will run this transition on the GlobalTransitionRunner.
-		/// If FALSE, will start the transition, but NOT run it,
-		/// meaning someone other than the GlobalTransitionRunner
-		/// will be responsible for updating this Transition to completion.
-		/// </param>
-		void StartTransition(float time, bool andRun);
-		
-		void UpdateTransition(float time, float deltaTime);
-		
-		void Complete();
-		
-		void CompleteEarly();
+    {
+        event Action<Transition> Completed; // TODO: replace with UnityEvent, possibly have a no-arg event option (for auto unbind)
 
-		/// <summary>
-		/// TRUE if the transition was ever started
-		/// </summary>
-		bool didTransitionStart { get; } 
+        /// <summary>
+        /// Name/id for this transition
+        /// </summary>
+        string transitionName { get; set; }
 
-		/// <summary>
-		/// TRUE if the transition reached completion. 
-		/// </summary>
-		bool isTransitionComplete { get; } 
+        /// <summary>
+        /// Starts and runs the transition
+        /// </summary>
+        void StartTransition();
 
-		/// <summary>
-		/// TRUE if the transition is running. False if it is finished, cancelled, was never started etc.
-		/// </summary>
-		bool isTransitionRunning { get; } 
-		
-		Transition WithCompletedAction(Action<Transition> a);
-		
-		Transition WithName(string name); 
-		
-		T WithName<T>(string name) where T : class, Transition;
-		
-	}
-	
+        /// <summary>
+        /// Starts the transition using the GlobalTransitionRunner
+        /// </summary>
+        /// <param name='time'>
+        /// Generally use Time.time, but having time as a param allow alternative time models
+        /// </param>
+        void StartTransition(float time);
+
+        /// <summary>
+        /// Starts the transition leaving the option to *NOT*
+        /// add the transition to the GlobalTransitionRunner.
+        /// This allows a transitions to start and update/control child transitions.
+        /// </summary>
+        /// <param name='time'>
+        /// Time.
+        /// </param>
+        /// <param name='andRun'>
+        /// If TRUE, will run this transition on the GlobalTransitionRunner.
+        /// If FALSE, will start the transition, but NOT run it,
+        /// meaning someone other than the GlobalTransitionRunner
+        /// will be responsible for updating this Transition to completion.
+        /// </param>
+        void StartTransition(float time, bool andRun);
+
+        void UpdateTransition(float time, float deltaTime);
+
+        void Complete();
+
+        void CompleteEarly();
+
+        /// <summary>
+        /// TRUE if the transition was ever started
+        /// </summary>
+        bool didTransitionStart { get; }
+
+        /// <summary>
+        /// TRUE if the transition reached completion.
+        /// </summary>
+        bool isTransitionComplete { get; }
+
+        /// <summary>
+        /// TRUE if the transition is running. False if it is finished, cancelled, was never started etc.
+        /// </summary>
+        bool isTransitionRunning { get; }
+
+        Transition WithCompletedAction(Action<Transition> a);
+    }
+
+    [EditDefine("TRANSITIONS_DEBUG_ENABLED",
+            "when enabled transitions should have better debug info")]
+    public static class TransitionExt
+    {
+        public static Transition WithName(this Transition t, string name)
+        {
+            if (t != null)
+            {
+                t.transitionName = name;
+            }
+            return t;
+        }
+
+        public static T WithName<T>(this T t, string name) where T : class, Transition
+        {
+            if (t != default(T))
+            {
+                t.transitionName = name;
+            }
+            return t;
+        }
+
+    }
+
 }
 
 
